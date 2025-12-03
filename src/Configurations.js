ee59c0028648d97f7b5346cd49ce15df0ef94683
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2025-12-02 18:27:48 -0600

    Modifique el archivo de configuravciones para leer los datos de la base de datos desde el .env

--- src/Configurations.js
+++ src/Configurations.js
@@ -1,6 +1,11 @@
-import 'dotenv/config'
+import { configDotenv } from "dotenv";
 
 export function configuraciones (){
+    configDotenv();
     const PORT = process.env.PORT
-    return {PORT}
+    const HOST = process.env.DB_HOST
+    const PASS = process.env.DB_PASS
+    const USER = process.env.DB_USER
+    const DATA = process.env.DB_DATA
+    return {PORT, HOST, PASS, USER, DATA}
 }
