4954369e38676567d9a0e9aa4d06db9593660dd1
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2025-12-03 18:40:37 -0600

    Se agrego el controlador de inserción de usuario

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,8 @@
-import {getUser} from '../models/user.model.js'
+import {getUser, signUp} from '../models/user.model.js'
+import { configuraciones } from '../Configurations.js';
+import bcrypt from 'bcrypt';
+
+const {SALT_ROUNDS} = configuraciones();
 
 export const signIn = async (req, res) => {
     try{
@@ -7,6 +11,20 @@ export const signIn = async (req, res) => {
         res.json(user)
     } catch(err){
         res.status(404).json({Message: "Error"})
+    }
+}
+
+export const Register_user = async (req, res) =>{
+    try {
+        const {name, email, password, estado} = req.body;
+        const password_hash = await bcrypt.hash(password, SALT_ROUNDS);
+        
+        const succesful = await signUp(name,email,password_hash, estado)
+        console.log(succesful)
+        res.json(succesful)
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({Message: error})
     }
 }
 
