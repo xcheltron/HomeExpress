53a3a2a38905d1bb94c5a4ef182da70688424a53
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2025-12-03 18:41:23 -0600

    Se agrego el modelo para insertar usuario

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,3 +8,13 @@ export const getUser = () =>{
         })
     })
 }
+
+export const signUp = (name, email, password_hash, estado) =>{
+    return new Promise ((resolve, reject) => {
+        console.log(name, email, password_hash, estado)
+        connection.query("CALL Register_user(?,?,?,?)",[name,email,password_hash, estado],(err,results)=>{
+            if (err) reject(err)
+                else resolve(results)
+        })
+    })
+}
