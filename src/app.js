9388676504b17f8ce82cf53730460f6187c9b70b
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2025-12-03 18:39:17 -0600

    Se modifico la estructura del index para agregar la funcions del multer y poder leeer json

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 //Todos los imports
 import express from 'express'
 import { configuraciones } from './Configurations.js'; //Imports de configuracion de dotenv
-
 import userRoutes from './routes/users.routes.js'
 
-const app = express();
-
-app.use('/users', userRoutes)
-
 const {PORT} = configuraciones();
+const app = express();
+app.use(express.json());
+
+app.use('/public', express.static('public'));
+app.use('/users', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Escuchando en el puerto ${PORT}`)
